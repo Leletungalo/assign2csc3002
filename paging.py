ecc049627d01a6de900177716d4217ffeb5e0a33
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-04-10 20:10:31 +0200

    start lru

--- paging.py
+++ paging.py
@@ -4,11 +4,11 @@ from random import *
 from sys import argv
 
 def fifo(size,pages):
-    print(pages)
     memory = []
     hit = 0
+    oldest = 0
     for i in range(0,len(pages)):
-        if (len(memory)) < 4:
+        if (len(memory)) < 3:
             
             if len(memory) == 0:
                 memory.append(pages[i])
@@ -34,18 +34,56 @@ def fifo(size,pages):
                 memory.append(pages[i])
                 memory.pop(0)
             hit += curhit2
-    print(hit)
-    print(memory)
+    print("faults: " , size - hit)
+    print("pages in memory: " , memory)
+    return size - hit
 
+def lru(size,pages):
+    memory = []
+    hit = 0
+    for i in range(0,len(pages)):
+        if len(memory) ==0:
+            memory.insert(0,pages[i])
+        else:
+            if len(memory) < 3:
+                curhit = 0
+                for j in range(0,len(memory)):
+                    if memory[j] == pages[i]:
+                        curhit +=1
+            
+                if curhit == 0:
+                    memory.insert(0,pages[i])
+                else:
+                    memory.remove(pages[i])
+                    memory.insert(0,pages[i])
+                hit += curhit
+
+            else:
+                curhit = 0
+                for j in range(0,len(memory)):
+                    if memory[j] == pages[i]:
+                        curhit +=1
+            
+                if curhit == 0:
+                    memory.insert(0,pages[i])
+                else:
+                    memory.remove(pages[i])
+                    memory.insert(0,pages[i])
+                hit += curhit
+
+    print(memory)
+    print(hit)
+            
 
 def main():
-    items = [9, 4, 6, 9, 3, 8, 7, 1, 8, 3]
-    #for i in range(0,10):
-     #  element = randint(1,9)
-     #  items.append(element)
-
+    items = [8, 5, 6, 2, 5, 3, 5, 4]
     size = int(argv[1])
-    fifo(size,items)
+    #for i in range(0,size):
+    #   element = randint(1,9)
+    #   items.append(element)
+    #print(fifo(size,items))
+    lru(size,items)
+    
 
 if __name__ == "__main__":
     if len(argv) != 2:
