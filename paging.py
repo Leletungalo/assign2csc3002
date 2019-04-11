048f184be0d6da4d71ac6104e145eb713e98bce8
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-04-11 10:20:22 +0200

    for test

--- paging.py
+++ paging.py
@@ -34,8 +34,6 @@ def fifo(size,pages):
                 memory.append(pages[i])
                 memory.pop(0)
             hit += curhit2
-    print("faults: " , size - hit)
-    print("pages in memory: " , memory)
     return size - hit
 
 def lru(size,pages):
@@ -66,24 +64,64 @@ def lru(size,pages):
             
                 if curhit == 0:
                     memory.insert(0,pages[i])
+                    memory.pop(len(memory) -1)
                 else:
                     memory.remove(pages[i])
                     memory.insert(0,pages[i])
+                    memory.pop(len(memory) -1)
                 hit += curhit
+    return size - hit
 
-    print(memory)
-    print(hit)
-            
+def OPT(size,pages):
+    memory = []
+    hit = 0
+    for i in range(0,len(pages)):
+        if len(memory) == 0:
+            memory.append(pages[i])
+
+        else:
+            if len(memory) < 3:
+                curhit = 0
+                for j in range(0,len(memory)):
+                    if memory[j] == pages[i]:
+                        curhit += 1
+
+                if curhit == 0:
+                    memory.append(pages[i])
+
+                else:
+                    counters = []
+                    con = 0
+                    distance = 0
+                    m = i
+                    for r in range(0,len(memory)):
+                        curElement = memory[r]
+                        for x in range(m,len(pages)):
+                            print(pages[x])
+                            #if (curElement != pages[x]):
+                            #    distance += 1
+                            #    break
+                        
+                        print("ljikoj",m)
+                        counters.append(distance)
+                        distance = 0
+                    print(counters)
+                hit = curhit
+    
+
+
 
 def main():
-    items = [8, 5, 6, 2, 5, 3, 5, 4]
+    #items = []
+    items = [8, 5, 5, 2, 5, 3, 5, 4]
     size = int(argv[1])
-    #for i in range(0,size):
-    #   element = randint(1,9)
-    #   items.append(element)
-    #print(fifo(size,items))
-    lru(size,items)
-    
+    for i in range(0,size):
+       element = randint(1,9)
+       items.append(element)
+    # may print 
+    #print("FIFO ",fifo(size,items)," page faults.")
+    #print ("LRU ",lru(size,items), "page faults.")
+    OPT(size,items)
 
 if __name__ == "__main__":
     if len(argv) != 2:
